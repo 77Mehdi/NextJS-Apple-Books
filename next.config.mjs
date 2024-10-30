0b4ac39f35317bea2a6b5b7c3d34e82612b344f8
77Mehdi <mgharyb917@gmail.com>
2024-10-30 07:19:41 +0100

    deploy-v5

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverRuntimeConfig: {
-        
-        port: process.env.PORT || 3000, 
-    },
-    
+    serverRuntimeConfig: {     
+        port: 'https://apple-books.onrender.com'
+    }, 
 };
 
 export default nextConfig;
